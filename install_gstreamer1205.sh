1c468a2f839e1848b72992b4e891b9b6e4ba00d4
sasgas <sasgas@castis.com>
2023-02-14 16:19:11 +0900

    minor fix

--- install_gstreamer1205.sh
+++ install_gstreamer1205.sh
@@ -6,6 +6,7 @@ yum -y install flex bison; yum -y clean all
 wget -nv --no-check-certificate --content-disposition https://github.com/GStreamer/gstreamer/archive/refs/tags/1.20.5.tar.gz
 tar xf gstreamer-1.20.5.tar.gz
 cd gstreamer-1.20.5
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
 meson -Dlibsoup:c_std=gnu99 build
 meson install -C build
 ldconfig
